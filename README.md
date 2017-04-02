406e207e803cda815b189ba055c7e1d07adbc521
Andrew Wagner <wagan8r@gmail.com>
2017-04-02 00:14:46 -0600

    Updated the app to have the issue body and assignee. Also added the readme.

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # github-issue-browser
+
+To run the app, first clone from GitHub 
+
+    git clone https://github.com/Wagan8r/github-issue-browser.git
+
+Make sure that you have [Node.js](https://nodejs.org/en/download/) installed. Then navigate to the project root and run:
+
+    npm start
+    
+Open up your favorite browser \*cough\*Chrome\*cough\* and go to:
+
+    http://localhost:3000
+    
+Enjoy!
--- src/components/Issue.js
+++ src/components/Issue.js
@@ -5,16 +5,21 @@ export default class Issue extends Component{
     render(){
         const {issue} = this.props;
         const reporter = issue ? issue.user.login : '';
+        const assignee = issue && issue.assignee ? issue.assignee.login : 'nobody';
 
         return (
             <table className="issue-container">
                 <tr>
-                    <td className="issue-stripe" rowSpan="3"></td>
+                    <td className="issue-stripe" rowSpan="4"></td>
                     <a className="issue-title" href={issue.html_url} target="_blank">{issue.number} - {issue.title}</a>
                     <td className="status-label">{issue.state}</td>
                 </tr>
                 <tr>
-                    <td className="user-label" colSpan="2">reported by {reporter}</td>
+                    <td className="user-label" colSpan="2">
+                        <div className="detail-left">assigned to {assignee}</div>
+                        <div className="detail-right">reported by {reporter}</div>
+                        <div className="last-detail-item"></div>
+                    </td>
                 </tr>
                 <tr>
                     <td className="detail-label" colSpan="2">
@@ -22,6 +27,9 @@ export default class Issue extends Component{
                         <div className="detail-right">Created on {issue.created_at}</div>
                         <div className="last-detail-item"></div>
                     </td>
+                </tr>
+                <tr>
+                    <td className="issue-body" colSpan={2}>{issue.body}</td>
                 </tr>
             </table>
         );
